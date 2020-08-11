32bedc6252214f2ecbe2e1066ae96a773f1886c1
tepkool01 <mvy1221@gmail.com>
2020-08-11 08:15:40 -0700

    Annual update. Refactoring some JS, removed dark mode from CSS, added copy content.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,26 @@
 "use strict";
-var clickEvent = "ontouchstart" in window ? "touchend" : "click",
-    classMethods = ["remove", "add"];
+let clickEvent = "ontouchstart" in window ? "touchend" : "click";
+let classMethods = ["remove", "add"];
 function vanillajQuery(id, textArr, className) {
-    var el = document.getElementsByTagName("html")[0];
-    var acbox = document.getElementById(id),
-        textNode = acbox.firstChild,
-        toggled = false;
+    let pageElements = document.getElementsByTagName("html")[0];
+    let acbox = document.getElementById(id);
+    let textNode = acbox.firstChild;
+    let toggled = false;
     acbox.addEventListener(
         clickEvent,
         function() {
-            var selector = Number((toggled = !toggled));
+            let selector = Number((toggled = !toggled));
             textNode.data = textArr[selector];
-            el.classList[classMethods[selector]](className);
+            pageElements.classList[classMethods[selector]](className);
         },
         false
     );
 }
-// function addDarkControl() {
-//     vanillajQuery("darkmode", ["Dark mode", "Normal mode"], "dark-mode");
-// }
 function addLeetControl() {
     vanillajQuery("leet-mode", ["View in Leet Mode", "View in Minimal Mode"], "leet-mode");
 }
 
-function getSkiData(id) {
-    const uri = "https://snowreporting.herokuapp.com/feed";
-    $.ajax({
-        url: uri,
-        method: "GET",
-        crossDomain: true,
-        data: {
-            resortId: 80
-        }
-    }).done(function(responseObject){
-
-    });
-}
-
 addLeetControl();
-// addDarkControl();
 
 window.onblur=function(){
     document.title="Zoe says come back!";
